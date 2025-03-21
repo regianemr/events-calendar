86c6e1245f05a59d2790c49188920b257cd5846a
Regiane Melo <regianemelo1995@gmail.com>
2025-03-21 12:11:06 -0300

    separando arquivo para conexao com banco de dados

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ import cors from 'cors'
 import 'dotenv/config'
 import express, { json } from 'express'
 
-import { connectToDB } from './config/db.js'
 import authRouter from './routes/auth.js'
 import eventRouter from './routes/events.js'
 
@@ -13,16 +12,11 @@ app.use(json())
 app.use('/auth', authRouter)
 app.use('/events', eventRouter)
 
-// Open Route - Public
 app.get('/', (req, res) => {
-  res.status(200).json({ msg: 'Seja bem-vindo!'})
+  res.status(200).json({ message: 'Seja bem-vindo!'})
 })
 
-const PORT = process.env.PORT || "5000"
-app.listen(PORT, () => {
-  connectToDB()
-  console.log("Server running on port", PORT)
-})
+export default app
 
   
 
